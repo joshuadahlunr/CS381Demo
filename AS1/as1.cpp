b3485cef485e323b9e40b038822b005e40e7b5a1
Joshua Dahl <joshuadahl@unr.edu>
2026-02-09 16:21:16 -0800

    Class 2/9/26

--- AS1/as1.cpp
+++ AS1/as1.cpp
@@ -1,8 +1,12 @@
 #include "AudioDevice.hpp"
+#include "Matrix.hpp"
+#include "RadiansDegrees.hpp"
+#include "Vector4.hpp"
 #include "raylib-cpp.hpp"
 #include <concepts>
 #include <iostream>
 #include <optional>
+#include <memory>
 
 // Callback
 auto PingButton() -> void {
@@ -22,6 +26,9 @@ void call_lambda(Tfunction func) {
     func();
 }
 
+// Heap access "anywhere"
+// Stack is access bottom
+
 int main() {
     raylib::Window window(300, 350, "As0");
     raylib::AudioDevice audio;
@@ -32,10 +39,25 @@ int main() {
     raylib::Music music("audio/price-of-freedom.mp3");
     music.Play();
 
+    auto A = raylib::Transform::CreateScale(3, 3, 3);
+    auto B = raylib::Transform::CreateRotateY(raylib::Degree(270));
+
+    auto transform = B * A;
+    raylib::Vector4 up = {0, 1, 0};
+    // transform * up; // HOW DO?
+
     std::optional<int> x = 5;
     if(x) {
         std::cout << x.value_or(0) << std::endl;
     }
+
+    int* p = new int;
+    delete p;
+
+    // std::unique_ptr<int> p2 = std::make_unique<int>(0);
+    // std::unique_ptr<int> p3 = std::move(p2);
+    std::shared_ptr<int> p2 = std::make_shared<int>(0);
+    std::shared_ptr<int> p3 = p2;
 
     // size_t counter = 0;
     // call_lambda([&counter](int) {
